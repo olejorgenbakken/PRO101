d3feda4fe995b7e1f771e2291f48e006de93a192
Assassinduck <darlion2301@hotmail.com>
2019-05-28 15:40:57 +0200

    Prototype drag and drop feature WIP
    First alomost finished prototype of D&D system for the kanban cards, WIP

---
+++ script/Drag_&_Drop.js
@@ -0,0 +1,48 @@
+
+var list = [
+  {
+    
+  },
+  {
+    listID: 1
+  }
+];
+
+var i = 0;
+
+ function addItem(){
+
+
+var div = document.querySelector(".card-");
+  var input = document.getElementById("cardInput");
+  var divNode = document.createElement("div");
+
+  if(input.value == ""){
+    return;
+  }
+  
+    i++;
+  console.log(i);
+
+  divNode.setAttribute('class',"card-" + i);
+  divNode.appendChild(document.createTextNode(input.value));
+  div.appendChild(divNode);
+  
+}
+
+
+var el = document.querySelector(".card-");
+var sortable = Sortable.create(el, {
+    group: "doing",
+    sort: true,
+});
+
+
+
+
+var el1 = document.getElementById("items_two");
+var sortable = Sortable.create(el1, {
+    group: "doing",
+    sort: true,
+
+});
