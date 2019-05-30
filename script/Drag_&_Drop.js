7c6dc6d32a18ef7e3c0dc9d1fba7dcb557cb9407
Assassinduck <darlion2301@hotmail.com>
2019-05-30 11:26:59 +0200

    Cleaned up some code
    cleaning up code and removing testing-based code

--- script/Drag_&_Drop.js
+++ script/Drag_&_Drop.js
@@ -1,131 +1,69 @@
 
-/*
-function NodeTree(text){
-  
-  
-  
+
+function NodeTree(text) {
+
+
   this.divElem = document.createElement("div");
   this.divElem.par = document.createElement("p");
   this.divElem.Node = this;
-  
+
   this.divElem.appendChild(this.divElem.par);
 
   this.divElem.par.innerText = text;
-
-  this.divElem.onclick = function(){
-
-  }
-
-
- 
-
-}
-*/
-
-
-var list1 = [
-  {
-    name: "julian",
-    age: 23,
-    gender: "male",
-  },
-  {
-    listID: 1
-  }
-];var list2 = [
-  {
-    
-  },
-  {
-    listID: 1
-  }
-];
-
-var i = 0;
-
- function addItem(){
-
-
-var div = document.querySelector(".card-");
-  var input = document.getElementById("cardInput");
-  var divNode = document.createElement("div");
-
-  if(input.value === ""){
-    return;
-  }
-  
-
-
-    i++;
-  console.log(i);
-
-  divNode.setAttribute('class',"card-" + i);
-  divNode.appendChild(document.createTextNode(input.value));
-  div.appendChild(divNode);
-  
 }
 
-var dragged;
 
-/*
-
-document.addEventListener("drag", function(event) {
-
-}, false);
-
-document.addEventListener("dragstart", function(event) {
-
-  dragged = event.target;
-
-  event.target.style.opacity = .5;
-}, false);
-
-document.addEventListener("dragend", function(event) {
-
-  event.target.style.opacity = "";
-}, false);
+  var dragged;
 
 
-document.addEventListener("dragover", function(event) {
+  document.addEventListener("drag", function (event) {
 
-  event.preventDefault();
-}, false);
+  }, false);
 
-document.addEventListener("dragenter", function(event) {
+  document.addEventListener("dragstart", function (event) {
 
-  if (event.target.className === "dropZone") {
-    event.target.style.background = "purple";
-  }
+    dragged = event.target;
 
-}, false);
+    event.target.style.opacity = .5;
+  }, false);
 
-document.addEventListener("dragleave", function(event) {
+  document.addEventListener("dragend", function (event) {
 
-  if (event.target.className === "dropZone") {
-    event.target.style.background = "";
-  }
-
-}, false);
-
-document.addEventListener("drop", function(event) {
-
-  event.preventDefault();
-
-  if (event.target.className == "dropZone") {
-    event.target.style.background = "";
-    dragged.parentNode.removeChild( dragged );
-    event.target.appendChild( dragged );
-
-   list1.ol
-  }
-}, false);
-*/
-
-el = document.getElementById("simpleList");
-sortable = new Sortable(el, {
-  group:"test",
-  sort:true,
+    event.target.style.opacity = "";
+  }, false);
 
 
+  document.addEventListener("dragover", function (event) {
 
-});
+    event.preventDefault();
+  }, false);
+
+  document.addEventListener("dragenter", function (event) {
+
+    if (event.target.className === "dropZone") {
+      event.target.style.background = "purple";
+    }
+
+  }, false);
+
+  document.addEventListener("dragleave", function (event) {
+
+    if (event.target.className === "dropZone") {
+      event.target.style.background = "";
+    }
+
+  }, false);
+
+  document.addEventListener("drop", function (event) {
+
+    event.preventDefault();
+
+    if (event.target.className == "dropZone") {
+      event.target.style.background = "";
+      dragged.parentNode.removeChild(dragged);
+      event.target.appendChild(dragged);
+
+    }
+  }, false);
+
+
