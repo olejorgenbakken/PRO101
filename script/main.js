6ba73084af02ba2d9ab97818232e9064b0320d41
filipjohnsen <filipjohnsen97@gmail.com>
2019-05-30 14:18:32 +0200

    Firebase Protype v2

--- script/main.js
+++ script/main.js
@@ -1,3 +1,19 @@
+//Firebase Config
+var firebaseConfig = {
+    apiKey: "AIzaSyCREyJtPwHTNRLFUgWyQcApGuZ4nTW3aiY",
+    authDomain: "pro101-425fc.firebaseapp.com",
+    databaseURL: "https://pro101-425fc.firebaseio.com",
+    projectId: "pro101-425fc",
+    storageBucket: "pro101-425fc.appspot.com",
+    messagingSenderId: "1025947228994",
+    appId: "1:1025947228994:web:69460f41a6cc0060"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+  var db = firebase.firestore();
+
 // tid og dat
 function getTime(input) {
     var today = new Date();
