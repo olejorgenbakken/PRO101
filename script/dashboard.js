04cb8182b4cb9cfad438c7f431311dab5b657fe8
olejorgenbakken <olejorgenbakken@gmail.com>
2019-05-31 13:24:57 +0200

    skal lage dashboard

--- script/dashboard.js
+++ script/dashboard.js
@@ -9,7 +9,7 @@ var bruker = [{
     image: "https://images.unsplash.com/photo-1556228841-7db5b6786a34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80"
 }];
 
-var wrapper = document.getElementById("nav");
+var wrapper = document.getElementById("profil");
 var profil = document.createElement("div");
 var brukerProfilbilde = document.createElement("img");
 var brukerProfilNavn = document.createElement("h2");
