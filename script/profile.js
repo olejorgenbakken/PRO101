08472de4e53cb5f65d5d5218a0b79519981be461
Ole Jørgen <37940048+olejorgenbakken@users.noreply.github.com>
2019-06-05 16:12:17 +0200

    landing page fixes, and start of project

--- script/profile.js
+++ script/profile.js
@@ -1,9 +1,17 @@
-var profile = {
-    userName: "Filip",
-    img: "/assets/img/000000.png"
-}
+var user = [{
+    username: "Filip",
+    img: "https://images.unsplash.com/photo-1509593419605-143becfcbae9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+}];
 
-var profileName = document.getElementById("profileText");
-var profileImg = document.getElementById("profileImg");
-profileImg.setAttribute("src", profile.img);
-profileName.innerHTML = profile.userName;
+var profile = document.getElementById("profile");
+var usernameText = document.createElement("h3");
+var userImgContainer = document.getElementById("profile-image");
+var userImg = document.createElement("img");
+
+userImgContainer.style.overflow = "hidden";
+userImg.setAttribute("src", user[0].img);
+usernameText.innerText = user[0].username;
+
+userImgContainer.appendChild(userImg);
+profile.appendChild(userImgContainer);
+profile.appendChild(usernameText);
