13978a2992dd505e97910caa445d62a9673cda56
Ole Jørgen <37940048+olejorgenbakken@users.noreply.github.com>
2019-06-06 13:04:42 +0200

    table done

---
+++ script/tables.js
@@ -0,0 +1,55 @@
+var project = [{
+    name: "Trip to somewhere"
+}];
+
+//lag board
+var board = [];
+
+var boardID = 0;
+
+var newBoardKnapp = document.getElementById("newBoardBtn");    
+newBoardKnapp.setAttribute("onclick", " return lagBoard()");
+
+var projectTitleWrp = document.getElementById("project-header");
+var projectTitle = document.createElement("input");
+projectTitle.className = "project-title";
+projectTitle.id = "project-title";
+projectTitle.type ="textarea";
+projectTitle.value = project[0].name;
+
+projectTitleWrp.appendChild(projectTitle);
+
+// lager en new liste med mulighet for å lage flere kort
+function lagBoard() {
+
+    board.push({
+        id: boardID,
+        navn: "board" + boardID,
+        antallBoardr: boardID,
+    });
+
+    var wrapper = document.getElementById("boards-wrapper");
+
+    var newBoard = document.createElement("div");
+    var newBoardTitle = document.createElement("input");
+    var deleteBoard = document.createElement("button");
+    deleteBoard.innerText = "Delete board";
+
+    newBoard.className = "board";
+    newBoard.id = "board" + boardID;
+    newBoardTitle.className = "board-title";
+    newBoardTitle.value = "New Board";
+    deleteBoard.setAttribute("onclick", " return deleteBoard(" + boardID + ")");
+
+    wrapper.appendChild(newBoard);
+    newBoard.appendChild(newBoardTitle);
+    newBoard.appendChild(deleteBoard);
+
+    boardID++;
+}
+
+// delete board
+function deleteBoard(boardID) {
+    var board = document.getElementById("board" + boardID);
+    board.remove("board" + boardID);
+}
